5d8dfcfdf2a563730e8e49375c6b77b0edae579b
gerrysabar <gerrysabar@gmail.com>
2024-08-28 23:54:22 +0700

    update unit test

--- server/router_test.go
+++ server/router_test.go
@@ -1,8 +1,11 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"go-todo/domain"
 	"go-todo/mock/service"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"time"
@@ -12,7 +15,7 @@ import (
 	"github.com/go-playground/assert"
 )
 
-func TestGetTodo(t *testing.T) {
+func TestTodoGetAllSuccess(t *testing.T) {
 	mocking := service.NewTodoService(t)
 	expected := []domain.Todo{
 		{
@@ -30,6 +33,7 @@ func TestGetTodo(t *testing.T) {
 			UpdatedAt: time.Now(),
 		},
 	}
+
 	RouterService := RouterService{
 		TodoService: mocking,
 	}
@@ -40,4 +44,30 @@ func TestGetTodo(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/v1/todos", nil)
 	router.ServeHTTP(w, req)
 	assert.Equal(t, 200, w.Code)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	mBody, _ := json.Marshal(expected)
+	result := "{\"data\":" + string(mBody) + "}"
+	assert.Equal(t, string(body), result)
+}
+
+func TestTodoGetAllFailed(t *testing.T) {
+	mocking := service.NewTodoService(t)
+	var expected []domain.Todo
+	RouterService := RouterService{
+		TodoService: mocking,
+	}
+
+	mocking.On("GetAll").Return(expected, errors.New("error from GetAll"))
+	router := SetupRouter(RouterService)
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/v1/todos", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 400, w.Code)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+	result := "{\"error\":\"error from GetAll\"}"
+	assert.Equal(t, string(body), result)
 }
